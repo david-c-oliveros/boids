54e89da4bebacb5b9967003dd0d57ab4d2a40e47
david-c-oliveros <david3oliveros@gmail.com>
2022-04-02 14:28:14 -0400

    More cleanup

--- DebugUtils.h
+++
@@ -1,9 +0,0 @@
-#include "olcPixelGameEngine.h"
-
-namespace debug
-{
-    void PrintVec(olc::vf2d vec, std::string name)
-    {
-        std::cout << name << ": " << "{ " << vec.x << ", " << vec.y << " }" << std::endl;
-    }
-}
